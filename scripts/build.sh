72e21bbece3bfa4236fdad7782c76f05c5617a7f
koksang <koksanggl@gmail.com>
2023-01-17 14:07:52 +0800

    [feature] edited build script, rename extended dockerfile

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-docker build --rm -t rayproject/ray:2.2.0-py310-cpu-extended -f ./infra/Dockerfile.extended .
+poetry export -o requirements.txt --without-hashes
+docker build --rm -t rayproject/ray:2.2.0-py310-cpu-extended -f ./infra/Dockerfile .
