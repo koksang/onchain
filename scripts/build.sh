c9611cad0db9946715039407da00f0e8c8e7d339
koksang <kok.sang@mindvalley.com>
2023-02-20 00:59:45 +0800

    [feature] updated ray image version

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 poetry export -o requirements.txt --without-hashes
-docker build --rm -t rayproject/ray:2.2.0-py310-cpu-extended -f ./infra/Dockerfile .
-docker tag rayproject/ray:2.2.0-py310-cpu-extended k3d-registry.localhost:5000/rayproject/ray:2.2.0-py310-cpu-extended
-docker push k3d-registry.localhost:5000/rayproject/ray:2.2.0-py310-cpu-extended
+docker build --rm -t k3d-registry.localhost:7777/rayproject/ray:2.3.0-py310-cpu-extended -f ./infra/Dockerfile .
+# docker tag rayproject/ray:2.2.0-py310-cpu-extended k3d-registry.localhost:5000/rayproject/ray:2.2.0-py310-cpu-extended
+docker push k3d-registry.localhost:7777/rayproject/ray:2.3.0-py310-cpu-extended
